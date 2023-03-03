848fdd0ab013111db213ce802a3ef5e2d79f6f10
John Slowik <john@jslowik.com>
2023-03-03 15:27:56 -0600

    Starting point code

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,16 +1,41 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const existingRole = cdk.aws_iam.Role.fromRoleArn(
+      this,
+      "ExistingRole",
+      "arn:aws:iam::939984321277:role/SpaceCloudStack-S3LambdaRoleB1E5D5C9-17FX54CGH1K7F"
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Functions
+    // Read function in Rust
+    const readFunction = new cdk.aws_lambda.Function(
+      this,
+      "ReadPeopleInSpaceRustFunction",
+      {
+        functionName: "read-people-in-space-rust",
+        runtime: cdk.aws_lambda.Runtime.PROVIDED_AL2,
+        memorySize: 128,
+        timeout: cdk.Duration.seconds(30),
+        code: cdk.aws_lambda.Code.fromAsset("../functions/out/"),
+        handler: "read-people-in-space-rust",
+        architecture: cdk.aws_lambda.Architecture.ARM_64,
+        role: existingRole,
+        logRetention: cdk.aws_logs.RetentionDays.ONE_DAY,
+      }
+    );
+
+    // Get existing bucket
+    const bucket = cdk.aws_s3.Bucket.fromBucketName(
+      this,
+      "SpaceCloudBucket",
+      "spaceclouddatabucket"
+    );
+
+    bucket.grantRead(readFunction);
   }
 }
