b133b53d38d2338287ad5553655d83abdd84a3b1
John Slowik <john@jslowik.com>
2023-03-08 16:58:38 -0600

    Cleanup

--- functions/readpeople/src/main.rs
+++ functions/readpeople/src/main.rs
@@ -1,7 +1,6 @@
-// use aws_sdk_s3::{Client, Region};
 use aws_config;
 use aws_sdk_s3 as s3;
-use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
+use lambda_http::{run, service_fn, Body, Error, Request, Response};
 use log::LevelFilter;
 use serde::{Deserialize, Serialize};
 use simple_logger::SimpleLogger;
@@ -28,11 +27,7 @@ enum Craft {
     Shenzhou15,
 }
 
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
+async fn function_handler(_event: Request) -> Result<Response<Body>, Error> {
     let config = aws_config::load_from_env().await;
     let client = s3::Client::new(&config);
     let data = client
@@ -42,16 +37,12 @@ async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
         .send()
         .await?;
 
-    ////log::info!("{}", data.content_length());
-
     let bytes = data.body.collect().await?.into_bytes();
     let response = std::str::from_utf8(&bytes)?;
     let temp: PeopleInSpaceResponse = serde_json::from_str(response).unwrap();
 
     log::info!("Updated date for found values: {}", temp.update_date);
 
-    // Return something that implements IntoResponse.
-    // It will be serialized to the right response event automatically by the runtime
     let resp = Response::builder()
         .status(200)
         .header("content-type", "application/json")
@@ -76,9 +67,7 @@ async fn main() -> Result<(), Error> {
 
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
-        // disable printing the name of the module in every log line.
         .with_target(false)
-        // disabling time is handy because CloudWatch will add the ingestion time.
         .without_time()
         .init();
 
