c6ae26c8c51657ebf7a94c39e46c70feb962e7f1
John Slowik <john@jslowik.com>
2023-03-23 08:37:27 -0500

    Read NEO added

--- functions/readnearearthobjects/src/main.rs
+++ functions/readnearearthobjects/src/main.rs
@@ -1,19 +1,20 @@
+use std::collections::HashMap;
+
 use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
 use log::LevelFilter;
 use reqwest;
 use serde::{Deserialize, Serialize};
 use simple_logger::SimpleLogger;
-mod models;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct ApiResponse {
     links: ApiResponseLink,
     element_count: i32,
-    near_earth_objects: Vec<NearEarthObjects>,
+    near_earth_objects: HashMap<String, Vec<NearEarthObject>>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct NearEarthObjects {
+struct NearEarthObject {
     id: String,
     neo_reference_id: String,
     name: String,
@@ -27,23 +28,30 @@ struct NearEarthObjects {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct ApiResponseLink {
-    next: String,
-    prev: String,
+struct NearEarthLink {
+    #[serde(alias = "self")]
     this: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct NearEarthObject {
-    id: String,
-    name: String,
-    nasa_jpl_url: String,
-    is_potentially_hazardous_asteroid: bool,
-    absolute_magnitude_h: f64,
-    estimated_diameter: EstimatedDiameter,
-    close_approach_data: Vec<CloseApproachData>,
-    is_sentry_object: bool,
+struct ApiResponseLink {
+    next: Option<String>,
+    prev: Option<String>,
+    #[serde(alias = "self")]
+    this: Option<String>,
 }
+
+// #[derive(Debug, Serialize, Deserialize)]
+// struct NearEarthObject {
+//     id: String,
+//     name: String,
+//     nasa_jpl_url: String,
+//     is_potentially_hazardous_asteroid: bool,
+//     absolute_magnitude_h: f64,
+//     estimated_diameter: EstimatedDiameter,
+//     close_approach_data: Vec<CloseApproachData>,
+//     is_sentry_object: bool,
+// }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct EstimatedDiameter {
@@ -92,13 +100,16 @@ async fn retrieveData() -> Result<ApiResponse, Error> {
     //     .map_err(Box::new)?;
 
     let json_response = client
-        .get("https://api.nasa.gov/neo/rest/v1/feed?")
+        .get("https://api.nasa.gov/neo/rest/v1/feed?start_date=2023-03-20&end_date=2023-03-20&api_key=OJjWbhF284SdhFoDq40D1WDNCtSfecyf6NjZqVyJ")
         .send()
         .await?
         .text()
         .await?;
 
+    println!("{:?}", json_response);
+
     let data = serde_json::from_str(&json_response)?;
+    println!("{:?}", data);
 
     Ok(data)
 }
@@ -107,9 +118,13 @@ async fn function_handler(_event: Request) -> Result<Response<Body>, Error> {
     let data = retrieveData().await?;
     let serialized_data = serde_json::to_string(&data).unwrap();
 
+    // TODO might be nice to have a factory for this?
     let resp = Response::builder()
         .status(200)
-        .header("content-type", "text/html")
+        .header("content-type", "application/json")
+        .header("Access-Control-Allow-Origin", "*")
+        .header("Access-Control-Allow-Methods", "GET")
+        .header("Access-Control-Allow-Headers", "Content-Type")
         .body(serialized_data.into())
         .map_err(Box::new)?;
     Ok(resp)
@@ -129,4 +144,17 @@ async fn main() -> Result<(), Error> {
         .init();
 
     run(service_fn(function_handler)).await
+}
+
+// Test for retrieveData function
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_retrieve_data() {
+        let data = retrieveData().await.unwrap();
+        println!("{:?}", data);
+        assert!(data.element_count > 0);
+    }
 }
