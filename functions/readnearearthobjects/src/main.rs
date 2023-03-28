4d94afef2a59a2a2a41108c69873ee3d84d9afe3
John Slowik <john@jslowik.com>
2023-03-28 14:20:17 -0500

    Moved model to shared project initial

--- functions/readnearearthobjects/src/main.rs
+++ functions/readnearearthobjects/src/main.rs
@@ -1,88 +1,13 @@
 use aws_config;
 use aws_sdk_s3 as s3;
-use aws_sdk_ssm::{Client as ssm_client, Error as ssm_error, Region, PKG_VERSION};
-use chrono::{DateTime, Utc};
-use s3::types::ByteStream;
-use std::collections::HashMap;
-
-use lambda_http::{run, service_fn, Body, Error, Request, Response};
+use aws_sdk_ssm::Client as ssm_client;
+use chrono::Utc;
+use lambda_http::{run, service_fn, Error, Request, Response};
 use log::LevelFilter;
 use reqwest;
-use serde::{Deserialize, Serialize};
+use s3::types::ByteStream;
+use shared::ApiResponse;
 use simple_logger::SimpleLogger;
-
-#[derive(Debug, Serialize, Deserialize)]
-struct ApiResponse {
-    links: ApiResponseLink,
-    element_count: i32,
-    near_earth_objects: HashMap<String, Vec<NearEarthObject>>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct NearEarthObject {
-    id: String,
-    neo_reference_id: String,
-    name: String,
-    nasa_jpl_url: String,
-    absolute_magnitude_h: f64,
-    estimated_diameter: EstimatedDiameter,
-    is_potentially_hazardous_asteroid: bool,
-    close_approach_data: Vec<CloseApproachData>,
-    is_sentry_object: bool,
-    links: ApiResponseLink,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct NearEarthLink {
-    #[serde(alias = "self")]
-    this: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct ApiResponseLink {
-    next: Option<String>,
-    prev: Option<String>,
-    #[serde(alias = "self")]
-    this: Option<String>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct EstimatedDiameter {
-    kilometers: EstimatedDiameterValues,
-    meters: EstimatedDiameterValues,
-    miles: EstimatedDiameterValues,
-    feet: EstimatedDiameterValues,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct EstimatedDiameterValues {
-    estimated_diameter_min: f64,
-    estimated_diameter_max: f64,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct CloseApproachData {
-    close_approach_date: String,
-    epoch_date_close_approach: i64,
-    relative_velocity: RelativeVelocity,
-    miss_distance: MissDistance,
-    orbiting_body: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct RelativeVelocity {
-    kilometers_per_second: String,
-    kilometers_per_hour: String,
-    miles_per_hour: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct MissDistance {
-    astronomical: String,
-    lunar: String,
-    kilometers: String,
-    miles: String,
-}
 
 async fn retrieve_data() -> Result<ApiResponse, Error> {
     let client = reqwest::Client::new();
